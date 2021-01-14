0a00fd469d70bb7ff53de450940a09158c3f6e5f
SerhiyDemchuk <sergeylobster@gmail.com>
2021-01-14 14:42:15 +0200

    91 react redux extensions fin

--- src/Redux/reduxStore.js
+++ src/Redux/reduxStore.js
@@ -1,10 +1,10 @@
-import {combineReducers, createStore, applyMiddleware} from 'redux';
+import {combineReducers, createStore, applyMiddleware, compose} from 'redux';
 import profileReducer from './reducers/profileReducer';
 import dialogueReducer from './reducers/dialogueReducer';
 import sidebarReducer from './reducers/sidebarReducer';
 import usersReducer from './reducers/usersReducer';
 import authReducer from './reducers/authReducer';
-import thunk from 'redux-thunk';
+import thunkMiddleware from 'redux-thunk';
 import { reducer as formReducer } from 'redux-form';
 import appReducer from './reducers/appReducer';
 
@@ -18,8 +18,9 @@ let reducers = combineReducers({
     form: formReducer
 });
 
-let store = createStore(reducers, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
 
-window.store = store;
+window.__store__ = store;
 
 export default store;
